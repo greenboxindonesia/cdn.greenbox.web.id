bfd2df3705ede0d529abd198fa066bc9165832cc
Anggo Suryo S <aank84@gmail.com>
2015-03-29 23:50:50 +0700

    replace all design new

--- js/custom.js
+++ js/custom.js
@@ -1,93 +1,67 @@
-
-/* Document ready
-================================================== */
-
-$(document).ready(function() {
-
-	/* subscribe form
-	================================================== */
-	$("#subscribe-submit").click(function(){
-		var valid = '';
-		var isr = ' required.';
-		var mail = $("#mail").val();
-		if (!mail.match(/^([a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,4}$)/i)) {
-			valid += '<br />Email'+isr;
-		}
-		if (valid!='') {
-			$("#response").fadeIn("slow");
-			$("#response").html("Required");
-		}
-		else {
-			var datastr ='&mail=' + mail;
-			$("#response").css("display", "block");
-			$("#response").html("Sending...");
-			$("#response").fadeIn("slow");
-			setTimeout("send('"+datastr+"')",2000);
-		}
+//Nav
+jQuery(document).ready(function ($){
+    jQuery('.navbar .nav > li > a, .footer-nav > li > a').click(function(){
+        jQuery.scrollTo( $(this).attr("href"), {
+			duration: 1000,
+			easing:'easeInOutExpo'			
+		});
 		return false;
-	});
-
-	/* image slider
-	================================================== */
-		var options = {
-			autoPlay: true,
-			animateStartingFrameIn: true,
-			autoPlayDelay: 3000,
-			transitionThreshold: 500,
-		};
-		var sequence = $("#image-slideshow").sequence(options).data("image-slideshow");
-
-	/* jQuery countdown
-	================================================== */
-
-		function parseDate(input) {
-			var parts = input.match(/(\d+)/g);
-			return new Date(parts[0], parts[1]-1, parts[2]);
-		}
-		$('#countdown').countdown({
-			layout:	'<div class="counter-block"><span id="days-num">{dn}</span><span id="days-desc">{dl}</span></div>' +
-				'<div class="counter-block"><span id="hours-num">{hn}</span><span id="hours-desc">{hl}</span></div>' +
-				'<div class="counter-block"><span id="min-num">{mn}</span><span id="min-desc">{ml}</span></div>' +
-				'<div class="counter-block"><span id="sec-num">{sn}</span><span id="sec-desc">{sl}</span></div>',
-			until: new Date(parseDate($('#countdown').data('date')))
-		});
-	
-	/* Twitter integration
-	================================================== */
-	jQuery(function($){
-		$("#ticker").tweet({
-			username: "ResetBlueArt",
-			page: 1,
-			count: 3,
-			loading_text: "loading ...",
-			template: "{text}"
-		}).bind("loaded", function() {
-			var ul = $(this).find(".tweet_list");
-			var ticker = function() {
-				setTimeout(function() {
-					ul.find('li:first').animate( {marginTop: '-20px'}, 900, function() {
-						$(this).detach().appendTo(ul).removeAttr('style');
-					});
-					ticker();
-				}, 5000);
-			};
-			ticker();
-		});
-	});
-
+    });
 });
 
-function send(datastr){
-	$.ajax({	
-		type: "POST",
-		url: "subscribe.php",
-		data: datastr,
-		cache: false,
-		success: function(html){
-		$("#response").fadeIn("slow");
-		$("#response").html(html);
-		setTimeout('$("#response").fadeOut("slow")',2000);
-	}
-	});
-}
+var tpj=jQuery;
+tpj.noConflict();
 
+tpj(document).ready(function() {
+
+if (tpj.fn.cssOriginal!=undefined)
+tpj.fn.css = tpj.fn.cssOriginal;
+
+tpj('.banner').revolution(
+	{
+		delay:5000,
+		startheight:520,
+		startwidth:1170,
+
+
+		hideThumbs:200,
+
+		thumbWidth:100,							// Thumb With and Height and Amount (only if navigation Tyope set to thumb !)
+		thumbHeight:50,
+		thumbAmount:5,
+
+		navigationType:"bullet",				// bullet, thumb, none
+		navigationArrows:"nexttobullets",				// nexttobullets, solo (old name verticalcentered), none
+
+		navigationStyle:"round-old",				// round,square,navbar,round-old,square-old,navbar-old, or any from the list in the docu (choose between 50+ different item), custom
+
+
+		navigationHAlign:"center",				// Vertical Align top,center,bottom
+		navigationVAlign:"bottom",					// Horizontal Align left,center,right
+		navigationHOffset:0,
+		navigationVOffset:-30,
+
+		soloArrowLeftHalign:"left",
+		soloArrowLeftValign:"top",
+		soloArrowLeftHOffset:20,
+		soloArrowLeftVOffset:0,
+
+		soloArrowRightHalign:"right",
+		soloArrowRightValign:"top",
+		soloArrowRightHOffset:20,
+		soloArrowRightVOffset:0,
+
+		touchenabled:"on",						// Enable Swipe Function : on/off
+		onHoverStop:"off",						// Stop Banner Timet at Hover on Slide on/off
+
+		stopAtSlide:-1,							// Stop Timer if Slide "x" has been Reached. If stopAfterLoops set to 0, then it stops already in the first Loop at slide X which defined. -1 means do not stop at any slide. stopAfterLoops has no sinn in this case.
+		stopAfterLoops:-1,						// Stop Timer if All slides has been played "x" times. IT will stop at THe slide which is defined via stopAtSlide:x, if set to -1 slide never stop automatic
+
+		hideCaptionAtLimit:0,					// It Defines if a caption should be shown under a Screen Resolution ( Basod on The Width of Browser)
+		hideAllCaptionAtLilmit:0,				// Hide all The Captions if Width of Browser is less then this value
+		hideSliderAtLimit:0,					// Hide the whole slider, and stop also functions if Width of Browser is less than this value
+
+		shadow:1,								//0 = no Shadow, 1,2,3 = 3 Different Art of Shadows  (No Shadow in Fullwidth Version !)
+		fullWidth:"off"							// Turns On or Off the Fullwidth Image Centering in FullWidth Modus
+	});
+});
